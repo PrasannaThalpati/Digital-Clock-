de045863118cd0b9078f01eb87c841cb677127c4
Prasanna Thalpati <69255097+PrasannaThalpati@users.noreply.github.com>
2021-07-23 15:52:23 +0530

    Add files via upload

---
+++ digital clock.py
@@ -0,0 +1,26 @@
+# Author :- Prasanna Thalpati 
+
+
+from tkinter import *
+import time
+
+def times():
+    current_time = time.strftime("%H:%M:%S")
+    clock.config(text=current_time)
+    clock.after(100,times)
+
+root= Tk()
+root.title("Digital Clock created by PRASANNA")
+root.geometry("500x250")
+clock=Label(root,font=("times",50,"bold"),bg="white")
+clock.grid(row=2,column=2,pady=25,padx=100)
+times()
+
+
+digi = Label(root,text="Digital Clock",font="times 24 bold")
+digi.grid(row=0,column=2)
+
+nota = Label(root,text="hours   minutes   seconds   " ,font="times 15 bold")
+nota.grid(row=3,column=2)
+
+root.mainloop()
